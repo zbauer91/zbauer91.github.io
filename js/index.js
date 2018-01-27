2437b1253b6c99324b065cc149f58b5a315d69bd
Zach Bauer <zbauer91@gmail.com>
2018-01-27 09:58:41 -0500

    added header that fades in

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let headClasses = document.getElementById('heading')
-  let subHeadClasses = document.getElementById('subHeading')
-
-  setTimeout(1000, () => {
-    head.addClass('')
-  })
+  displayHomePage()
 })
+
+function displayHomePage () {
+  let head = document.getElementById('heading')
+  let subHead = document.getElementById('subHeading')
+  let nav = document.getElementById('buttonContainer')
+
+  setTimeout(() => {
+    // console.log('buttons')
+    nav.classList.remove('hidden')
+    nav.classList.add('fadeInDown')
+  }, 500)
+
+  setTimeout(() => {
+    // console.log('header')
+    head.classList.remove('hidden')
+    head.classList.add('fadeInRight')
+  }, 1500)
+
+  setTimeout(() => {
+    // console.log('subheader')
+    subHead.classList.remove('hidden')
+    subHead.classList.add('fadeInLeft')
+  }, 2500)
+}
