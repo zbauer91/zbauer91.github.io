05e40c6f617e7998be342e9326fdc5bc6202f6ec
Zach Bauer <zbauer91@gmail.com>
2018-02-20 17:01:23 -0500

    removed another demo site, added jarallax. I think Im finally happy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(() => {
-  // Multiple instances, no options needed. keeping jQuery for now
-  $('.parallax').parallax()
-
   // Dropdown Initializer
   var dd = document.querySelector('.dropdown-trigger')
   var instance = M.Dropdown.init(dd, {
