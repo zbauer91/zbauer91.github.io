9720e23f511a43520572f9698f7543225fcad8fc
Zach Bauer <zbauer91@gmail.com>
2018-02-21 18:56:13 -0500

    This isn't going well/I'm unsatisfied....updating to node for now >.>

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+const lifeStory = require('../assets/text/lifeStory.json')
+
 document.addEventListener('DOMContentLoaded', function () {
-  // Dropdown Initializer
-  var dd = document.querySelectorAll('.dropdown-trigger')
-  var instance = M.Dropdown.init(dd, {
+  // Initializers
+  // Dropdown
+  let dd = document.querySelectorAll('.dropdown-trigger')
+  let instance = M.Dropdown.init(dd, {
     alignment: 'right',
     coverTrigger: false,
     constrainWidth: false
   })
+
+  // Main Code
+  // media query list for DOM manipulation
+  var smallPage = window.matchMedia('(max-width: 600px)')
+  var mediumPage = window.matchMedia('(max-width: 992px)')
+  var largePage = window.matchMedia('(min-width: 993px)')
+  var xLargePage = window.matchMedia('(min-width: 1200px)')
+
+  function navBarHalfDropdown () {
+    // code
+  }
+  function navBarAllDropdown () {
+    // code
+  }
 })
