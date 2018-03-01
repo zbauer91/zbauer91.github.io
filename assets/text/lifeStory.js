26cb54077d5cc48d37e436f4652e89fbe6cf1cdb
Zach Bauer <zbauer91@gmail.com>
2018-03-01 08:27:31 -0500

    programmatically added navbar links from common js file

---
+++ assets/text/lifeStory.js
@@ -0,0 +1,99 @@
+var lifeStory = {
+  navBar: {
+    linkedIn: {
+      link: 'https://www.linkedin.com/in/zachbauer',
+      icon: 'fab fa-linkedin',
+      text: 'LinkedIn'
+    },
+    resume: {
+      link: 'http://pixelrabbit.me/assets/resume.pdf',
+      icon: 'fas fa-file-alt',
+      inner: 'Resume'
+    },
+    gitHub: {
+      link: 'https://www.github.com/zbauer91',
+      icon: 'fab fa-github',
+      inner: 'GitHub'
+    },
+    email: {
+      link: 'mailto:zbauer91@gmail.com',
+      icon: 'fas fa-envelope',
+      text: 'Email'
+    },
+    phone: {
+      link: 'tel:16145193442',
+      icon: 'fas fa-phone',
+      text: 'Phone'
+    },
+    contact: {
+      link: '#!',
+      icon: 'fas fa-address-card',
+      text: 'Contact'
+    }
+  },
+  landing: {
+    header: 'Zach Bauer',
+    subHeader: '&lt;Classy Web Developer/&gt;'
+  },
+  about: {
+    photo: '#',
+    tagline: 'An expert at turning caffeine and lack of sleep into websites',
+    brandStatement:
+      "I'm always on the lookout for new, creative ways to solve problems. Whether that be in the work I do professionally, or at home, I'm a practiced MacGyver of programming."
+  },
+  skills: [
+    'React.js',
+    'Vue.js',
+    'HTML5',
+    'CSS3',
+    'Materialize',
+    'Bootstrap',
+    'Handlebars.js',
+    'Node.js',
+    'Express.js',
+    'Axios',
+    'Ruby',
+    'Rails',
+    'Devise Auth',
+    'MongoDB',
+    'PostgreSQL',
+    'SAP HANA',
+    'Markdown',
+    'Java',
+    'Python 3',
+    'Whitespace'
+  ],
+  projects: {
+    hanoi: {
+      title: 'Tower of Hanoi',
+      description:
+        'A non-responsive, vanilla HTML, CSS and JavaScript clone of the popular Tower of Hanoi. The game can auto-solve itself inside the dev console. Best played on a full-size computer.',
+      deployedLink: 'http://pixelrabbit.me/Tower-of-Hanoi/',
+      githubBack: 'https://github.com/zbauer91/Tower-of-Hanoi'
+    },
+    motto: {
+      title: 'MottoTrack',
+      description:
+        'Vue.js + Vuetify application to CRUD entries into a remote database. Concepts of Vue.js and implementation achieved in four days.',
+      deployedLink: 'http://pixelrabbit.me/MottoTrack/#/',
+      githubFront: 'https://github.com/zbauer91/MottoTrack',
+      githubBack: 'https://github.com/zbauer91/project-two-api'
+    },
+    barber: {
+      title: 'BarberHub DC',
+      description:
+        'A review aggregator to help DC find a quality barber. It is a full-stack MERN application with an express API that connects to Foursquare and Google Maps. It was written as a group project with two other General Assembly students.',
+      deployedLink: 'http://pixelrabbit.me/barber-find-dc/',
+      githubFront: 'https://github.com/zbauer91/barber-find-dc',
+      githubBack: 'https://github.com/zbauer91/barber-find-api'
+    },
+    habit: {
+      title: 'HabiTrack',
+      description:
+        'A React-on-Rails application that will allow users to easily set up and record habits throughout the day, week, and month. It will have features to allow for separate user accounts, tracking mileage on certain exercises, and adding/removing custom habits.',
+      deployedLink: '#',
+      githubBack: '#',
+      githubFront: '#'
+    }
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var data = lifeStory
   // Initializers
   // Dropdown
   let dd = document.querySelectorAll('.dropdown-trigger')
@@ -7,4 +8,14 @@ document.addEventListener('DOMContentLoaded', function () {
     coverTrigger: false,
     constrainWidth: false
   })
+
+  // Add information into document:
+  document.querySelector('#header').innerHTML = data.landing.header
+  document.querySelector('#subHeader').innerHTML = data.landing.subHeader
+
+  // programmatically populate links to avoid duplication in as many places as possible
+  for (let element in data.navBar) {
+    listItem = document.querySelector(`#${element}`)
+    listItem.innerHTML = `<a href="${element.link}" target="_blank" class="nav-link"><i class="${element.icon}"></i>${element.text}</a>`
+  }
 })
