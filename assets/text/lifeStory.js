bfb0f42efd75b1c9f3a0eb23d1955de9b11c75ad
Zach Bauer <zbauer91@gmail.com>
2018-03-01 11:30:49 -0500

    programmatically generate everything!

--- assets/text/lifeStory.js
+++ assets/text/lifeStory.js
@@ -3,32 +3,27 @@ var lifeStory = {
     linkedIn: {
       link: 'https://www.linkedin.com/in/zachbauer',
       icon: 'fab fa-linkedin',
-      text: 'LinkedIn'
+      text: '  LinkedIn'
     },
     resume: {
       link: 'http://pixelrabbit.me/assets/resume.pdf',
       icon: 'fas fa-file-alt',
-      text: 'Resume'
+      text: '  Resume'
     },
     gitHub: {
       link: 'https://www.github.com/zbauer91',
       icon: 'fab fa-github',
-      text: 'GitHub'
+      text: '  GitHub'
     },
     email: {
       link: 'mailto:zbauer91@gmail.com',
       icon: 'fas fa-envelope',
-      text: 'Email'
+      text: '  Email'
     },
     phone: {
       link: 'tel:16145193442',
       icon: 'fas fa-phone',
-      text: 'Phone'
-    },
-    contact: {
-      link: '#!',
-      icon: 'fas fa-address-card',
-      text: 'Contact'
+      text: '  Phone'
     }
   },
   landing: {
@@ -36,8 +31,9 @@ var lifeStory = {
     subHeader: '&lt;Classy Web Developer/&gt;'
   },
   about: {
-    photo: '#',
-    tagline: 'An expert at turning caffeine and lack of sleep into websites',
+    personalPhoto: './assets/images/photo.jpg',
+    photoAlt: "A photo of Zach's cute face!",
+    tagline: 'An expert at turning caffeine and lack of sleep into code',
     brandStatement:
       "I'm always on the lookout for new, creative ways to solve problems. Whether that be in the work I do professionally, or at home, I'm a practiced MacGyver of programming."
   },
@@ -68,32 +64,57 @@ var lifeStory = {
       title: 'Tower of Hanoi',
       description:
         'A non-responsive, vanilla HTML, CSS and JavaScript clone of the popular Tower of Hanoi. The game can auto-solve itself inside the dev console. Best played on a full-size computer.',
-      deployedLink: 'http://pixelrabbit.me/Tower-of-Hanoi/',
-      githubBack: 'https://github.com/zbauer91/Tower-of-Hanoi'
+      deploy: {
+        link: 'http://pixelrabbit.me/Tower-of-Hanoi/',
+        title: 'Game'
+      },
+      back: {
+        link: 'https://github.com/zbauer91/Tower-of-Hanoi',
+        title: 'GitHub Repo'
+      }
     },
     motto: {
       title: 'MottoTrack',
       description:
         'Vue.js + Vuetify application to CRUD entries into a remote database. Concepts of Vue.js and implementation achieved in four days.',
-      deployedLink: 'http://pixelrabbit.me/MottoTrack/#/',
-      githubFront: 'https://github.com/zbauer91/MottoTrack',
-      githubBack: 'https://github.com/zbauer91/project-two-api'
+      deploy: {
+        link: 'http://pixelrabbit.me/MottoTrack/#/',
+        title: 'Application'
+      },
+      front: {
+        link: 'https://github.com/zbauer91/MottoTrack',
+        title: 'Frontend Repo'
+      },
+      back: {
+        link: 'https://github.com/zbauer91/project-two-api',
+        title: 'Backend Repo'
+      }
     },
     barber: {
       title: 'BarberHub DC',
       description:
         'A review aggregator to help DC find a quality barber. It is a full-stack MERN application with an express API that connects to Foursquare and Google Maps. It was written as a group project with two other General Assembly students.',
-      deployedLink: 'http://pixelrabbit.me/barber-find-dc/',
-      githubFront: 'https://github.com/zbauer91/barber-find-dc',
-      githubBack: 'https://github.com/zbauer91/barber-find-api'
+      deploy: {
+        link: 'http://pixelrabbit.me/barber-find-dc/',
+        title: 'Application'
+      },
+      front: {
+        link: 'https://github.com/zbauer91/barber-find-dc',
+        title: 'Frontend Repo'
+      },
+      back: {
+        link: 'https://github.com/zbauer91/barber-find-api',
+        title: 'Backend Repo'
+      }
     },
     habit: {
       title: 'HabiTrack',
       description:
         'A React-on-Rails application that will allow users to easily set up and record habits throughout the day, week, and month. It will have features to allow for separate user accounts, tracking mileage on certain exercises, and adding/removing custom habits.',
-      deployedLink: '#',
-      githubBack: '#',
-      githubFront: '#'
+      deploy: {
+        link: '',
+        title: 'Links coming soon!'
+      }
     }
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var data = lifeStory
+  const data = lifeStory
   // Initializers
   // Dropdown
   let dd = document.querySelectorAll('.dropdown-trigger')
@@ -10,13 +10,51 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 
   // Add information into document:
-  document.querySelector('#header').innerHTML = data.landing.header
-  document.querySelector('#subHeader').innerHTML = data.landing.subHeader
+  for (let element in data.landing) {
+    document.querySelector(`#${element}`).innerHTML = data.landing[element]
+  }
 
   // programmatically populate links to avoid duplication in as many places as possible
   for (let element in data.navBar) {
-    listItem = document.querySelector(`#${element}`)
+    let listItem = document.querySelector(`#${element}`)
     object = data.navBar[element]
     listItem.innerHTML = `<a href="${object.link}" target="_blank" class="nav-link"><i class="${object.icon}"></i>${object.text}</a>`
   }
+  // photo
+  let photo = document.querySelector('.personal-photo')
+  photo.setAttribute('src', data.about.personalPhoto)
+  photo.setAttribute('alt', data.about.photoAlt)
+  // tagline and body
+  document.querySelector('.tagline').innerHTML = data.about.tagline
+  document.querySelector('.brand-statement').innerHTML =
+    data.about.brandStatement
+
+  // skills section
+  let skills = document.querySelector('.skill-row')
+  data.skills.forEach(function (element) {
+    var newSkill = document.createElement('div')
+    newSkill.classList.add('skill')
+    newSkill.innerHTML = element
+    skills.appendChild(newSkill)
+  })
+
+  // projects section
+  let projects = document.querySelectorAll('.project')
+
+  projects.forEach(function (element) {
+    // pulls in object related to this element
+    let obj = data.projects[element.getAttribute('id')]
+    element.querySelector('.project-title').innerHTML = obj.title
+    element.querySelector('.project-text').innerHTML = obj.description
+    var links = ['deploy', 'front', 'back']
+    for (i in links) {
+      let anchor = element.querySelector(`.${links[i]}`)
+      if (anchor) {
+        console.log(obj[links[i]].link)
+        console.log(obj[links[i]].title)
+        anchor.setAttribute('href', obj[links[i]].link)
+        anchor.innerHTML = obj[links[i]].title
+      }
+    }
+  })
 })
